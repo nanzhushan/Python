d8e7e5469f080aa2efdc4596606d9f94f6022da7
Long Ge <624867243@qq.com>
2016-12-18 21:22:47 +0800

    with as

--- 基础知识/异常处理/V1/try-expect-as.py
+++ 基础知识/异常处理/V1/try-expect-as.py
@@ -2,7 +2,7 @@
 #coding:utf8
 # as : 定义异常实例(except IOError as e)
 try:
-	pritn "try try try"
+	print "try try try"
 
 except Exception as e:
 	print "接受异常，并把这个异常储存起来，方便以后打出到文件或者其他地方",e
--- 基础知识/异常处理/V1/with-as.py
+++ 基础知识/异常处理/V1/with-as.py
@@ -1,15 +1,18 @@
 #coding:utf-8
 #下面两个语句等价，语法是不是简洁多了
+#使用with ....as...  极大的简化了每次写finally的工作,使代码更加优雅
 
-with open('d:\\1.txt','r') as f:   ##第一语句
+with open('d:\\1.txt','r') as f:   ##第一语句   #该语句 在处理完了之后会自动 会关闭文件.
     print f
-    
+
+
+########################    
 try:                                   #第二语句        
     f1 = open('d"\\1.txt','r')
 except:
     pass
 else:
-    print "hello"
+    print "没有文件，也就没有不需要关闭"
 finally:
-    f.close()
+    f.close()    #有文件才能进行关闭
     
