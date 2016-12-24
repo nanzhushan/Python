8c5d4b0c33068f44a24dba463a81ec87d620c86f
knight <624867243@qq.com>
2016-12-24 13:21:25 +0800

    dd

---
+++ 基础知识/三元操作符/v2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#coding:utf8
+x,y = 4,5
+print x
+print y
+if x < y:
+	small = x
+else:
+	small = y
+
+
+print "the samll num is :",small	
---
+++ 基础知识/三元操作符/v3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+#coding:utf8
+x = 1
+y = 2
+
+b = x if x < y else y
+print b
